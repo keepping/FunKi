7f9c570151a3f82c414e29b97c40f635332dba50
montotone <zgjxfuqingwang@gmail.com>
2017-02-22 17:22:06 +0800

    add-->changeColor

--- app/src/main/java/com/hifunki/www/funki/ui/activity/VisitorFillActivity.java
+++ app/src/main/java/com/hifunki/www/funki/ui/activity/VisitorFillActivity.java
@@ -1,5 +1,6 @@
 package com.hifunki.www.funki.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextPaint;
 import android.view.View;
@@ -39,8 +40,6 @@ public class VisitorFillActivity extends BaseActivity {
     TextView tvSecretSex;
     @BindView(R.id.etInputAge)
     EditText etInputAge;
-    @BindView(R.id.textView)
-    TextView textView;
     @BindView(R.id.tvVisitorConfirm)
     TextView tvVisitorConfirm;
     @BindView(R.id.llSkip)
@@ -48,6 +47,8 @@ public class VisitorFillActivity extends BaseActivity {
     @BindView(R.id.activity_visitor_fill)
     LinearLayout activityVisitorFill;
 
+    private int sex;
+    private int sexOrientation;
 
     @Override
     protected int getViewResId() {
@@ -91,38 +92,50 @@ public class VisitorFillActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.tvBoy, R.id.tvGirl, R.id.tvThirdSex, R.id.tvLoveSameSex, R.id.tvLoveDifferentSex, R.id.tvNotCareSex, R.id.tvLoveBothSex, R.id.tvSecretSex, R.id.etInputAge, R.id.textView, R.id.tvVisitorConfirm, R.id.llSkip, R.id.activity_visitor_fill})
+    @OnClick({R.id.tvBoy, R.id.tvGirl, R.id.tvThirdSex, R.id.tvLoveSameSex, R.id.tvLoveDifferentSex, R.id.tvNotCareSex, R.id.tvLoveBothSex, R.id.tvSecretSex, R.id.etInputAge, R.id.tvVisitorConfirm, R.id.llSkip, R.id.activity_visitor_fill})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tvBoy:
                 changeTvStyle(tvBoy, tvGirl, tvThirdSex);
+                sex = 1;
                 break;
             case R.id.tvGirl:
                 changeTvStyle(tvGirl, tvBoy, tvThirdSex);
+                sex = 2;
                 break;
             case R.id.tvThirdSex:
                 changeTvStyle(tvThirdSex, tvGirl, tvBoy);
+                sexOrientation = 1;
                 break;
             case R.id.tvLoveSameSex:
                 changeTvStyle(tvLoveSameSex, tvLoveDifferentSex, tvNotCareSex, tvLoveBothSex, tvSecretSex);
+                sexOrientation = 2;
                 break;
             case R.id.tvLoveDifferentSex:
                 changeTvStyle(tvLoveDifferentSex, tvLoveSameSex, tvNotCareSex, tvLoveBothSex, tvSecretSex);
+                sexOrientation = 3;
                 break;
             case R.id.tvNotCareSex:
                 changeTvStyle(tvNotCareSex, tvLoveSameSex, tvLoveDifferentSex, tvLoveBothSex, tvSecretSex);
+                sexOrientation = 4;
                 break;
             case R.id.tvLoveBothSex:
                 changeTvStyle(tvLoveBothSex, tvLoveSameSex, tvLoveDifferentSex, tvNotCareSex, tvSecretSex);
+                sexOrientation = 5;
                 break;
             case R.id.tvSecretSex:
                 changeTvStyle(tvSecretSex, tvLoveSameSex, tvLoveDifferentSex, tvNotCareSex, tvLoveBothSex);
+                sexOrientation = 6;
                 break;
             case R.id.etInputAge:
                 break;
-            case R.id.textView:
-                break;
             case R.id.tvVisitorConfirm:
+                if (sex == 0 && sexOrientation == 0) {
+                    return;
+                }
+                Intent intent = new Intent();
+                intent.setClass(VisitorFillActivity.this, LoginActivity.class);
+                startActivity(intent);
                 break;
             case R.id.llSkip:
 
@@ -132,6 +145,12 @@ public class VisitorFillActivity extends BaseActivity {
         }
     }
 
+    /**
+     * 改变字体style
+     *
+     * @param tvs
+     * @param textViews
+     */
     private void changeTvStyle(TextView tvs, TextView... textViews) {
 
         tvs.setTextColor(getResources().getColor(R.color.vistorTvClickbg));
