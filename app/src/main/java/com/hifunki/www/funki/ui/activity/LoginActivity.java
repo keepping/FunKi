59dbb11763e2545e90bbaba125c7d6aabb4e196f
montotone <zgjxfuqingwang@gmail.com>
2017-02-22 17:11:13 +0800

    add-->changeColor

---
+++ app/src/main/java/com/hifunki/www/funki/ui/activity/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.hifunki.www.funki.ui.activity;
+
+import com.hifunki.www.funki.R;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @Override
+    protected int getViewResId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void loadDatas() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
--- app/src/main/java/com/hifunki/www/funki/ui/activity/VisitorFillActivity.java
+++ app/src/main/java/com/hifunki/www/funki/ui/activity/VisitorFillActivity.java
@@ -1,7 +1,9 @@
 package com.hifunki.www.funki.ui.activity;
 
+import android.os.Bundle;
 import android.text.TextPaint;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,6 +13,7 @@ import com.hifunki.www.funki.util.StatusBarUtil;
 import com.hifunki.www.funki.util.TextUtil;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class VisitorFillActivity extends BaseActivity {
@@ -18,14 +21,33 @@ public class VisitorFillActivity extends BaseActivity {
 
     @BindView(R.id.tbVisitor)
     TitleBar tbVisitor;
+    @BindView(R.id.tvBoy)
+    TextView tvBoy;
+    @BindView(R.id.tvGirl)
+    TextView tvGirl;
+    @BindView(R.id.tvThirdSex)
+    TextView tvThirdSex;
+    @BindView(R.id.tvLoveSameSex)
+    TextView tvLoveSameSex;
+    @BindView(R.id.tvLoveDifferentSex)
+    TextView tvLoveDifferentSex;
+    @BindView(R.id.tvNotCareSex)
+    TextView tvNotCareSex;
+    @BindView(R.id.tvLoveBothSex)
+    TextView tvLoveBothSex;
+    @BindView(R.id.tvSecretSex)
+    TextView tvSecretSex;
+    @BindView(R.id.etInputAge)
+    EditText etInputAge;
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.tvVisitorConfirm)
+    TextView tvVisitorConfirm;
+    @BindView(R.id.llSkip)
+    LinearLayout llSkip;
     @BindView(R.id.activity_visitor_fill)
     LinearLayout activityVisitorFill;
-    @BindView(R.id.tv)
-    TextView tv;
-    @BindView(R.id.tvBig)
-    TextView tvBig;
-    @BindView(R.id.tvNormal)
-    TextView tvNormal;
+
 
     @Override
     protected int getViewResId() {
@@ -45,7 +67,7 @@ public class VisitorFillActivity extends BaseActivity {
 
         textView.setTextColor(getResources().getColor(R.color.titleText));
 
-        TextUtil.setFzNormalTypeFace(this,textView);
+        TextUtil.setFzNormalTypeFace(this, textView);
 
 //        http://blog.csdn.net/to_cm/article/details/6002812
         TextPaint paint = textView.getPaint();
@@ -62,22 +84,63 @@ public class VisitorFillActivity extends BaseActivity {
     }
 
 
-    @OnClick({R.id.tbVisitor, R.id.activity_visitor_fill, R.id.tv, R.id.tvBig, R.id.tvNormal})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.tvBoy, R.id.tvGirl, R.id.tvThirdSex, R.id.tvLoveSameSex, R.id.tvLoveDifferentSex, R.id.tvNotCareSex, R.id.tvLoveBothSex, R.id.tvSecretSex, R.id.etInputAge, R.id.textView, R.id.tvVisitorConfirm, R.id.llSkip, R.id.activity_visitor_fill})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.tv:
+            case R.id.tvBoy:
+                changeTvStyle(tvBoy, tvGirl, tvThirdSex);
                 break;
-            case R.id.tvBig:
+            case R.id.tvGirl:
+                changeTvStyle(tvGirl, tvBoy, tvThirdSex);
+                break;
+            case R.id.tvThirdSex:
+                changeTvStyle(tvThirdSex, tvGirl, tvBoy);
+                break;
+            case R.id.tvLoveSameSex:
+                changeTvStyle(tvLoveSameSex, tvLoveDifferentSex, tvNotCareSex, tvLoveBothSex, tvSecretSex);
+                break;
+            case R.id.tvLoveDifferentSex:
+                changeTvStyle(tvLoveDifferentSex, tvLoveSameSex, tvNotCareSex, tvLoveBothSex, tvSecretSex);
+                break;
+            case R.id.tvNotCareSex:
+                changeTvStyle(tvNotCareSex, tvLoveSameSex, tvLoveDifferentSex, tvLoveBothSex, tvSecretSex);
+                break;
+            case R.id.tvLoveBothSex:
+                changeTvStyle(tvLoveBothSex, tvLoveSameSex, tvLoveDifferentSex, tvNotCareSex, tvSecretSex);
+                break;
+            case R.id.tvSecretSex:
+                changeTvStyle(tvSecretSex, tvLoveSameSex, tvLoveDifferentSex, tvNotCareSex, tvLoveBothSex);
+                break;
+            case R.id.etInputAge:
+                break;
+            case R.id.textView:
+                break;
+            case R.id.tvVisitorConfirm:
+                break;
+            case R.id.llSkip:
 
-                break;
-            case R.id.tvNormal:
-
-                break;
-            case R.id.tbVisitor:
                 break;
             case R.id.activity_visitor_fill:
                 break;
         }
     }
 
+    private void changeTvStyle(TextView tvs, TextView... textViews) {
+
+        tvs.setTextColor(getResources().getColor(R.color.vistorTvClickbg));
+        tvs.setBackground(getResources().getDrawable(R.drawable.visitor_tv_click_bg));
+
+        for (TextView tv : textViews) {
+            tv.setTextColor(getResources().getColor(R.color.vistorTvbg));
+            tv.setBackground(getResources().getDrawable(R.drawable.visitor_tv_bg));
+        }
+
+    }
 }
