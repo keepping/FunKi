e9b276620a1cea7858f51fcb65d1e097b5f50c0b
montotone <zgjxfuqingwang@gmail.com>
2017-03-07 11:34:15 +0800

    add

---
+++ app/src/main/java/com/hifunki/funki/module/home/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.hifunki.funki.module.home;
+
+/**
+ * 在此写用途
+ *
+ * @author monotone
+ * @version V1.0 <描述当前版本功能>
+ * @value com.hifunki.funki.module.home.HomeActivity.java
+ * @link
+ * @since 2017-03-07 11:33:33
+ */
+public class HomeActivity {
+}
