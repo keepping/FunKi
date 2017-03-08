23b8c9947da896176ba1401cd523ad0a2768c6a9
montotone <zgjxfuqingwang@gmail.com>
2017-03-08 16:20:32 +0800

    add-->finish pwd graph

--- app/src/main/java/com/hifunki/funki/module/login/PwdGraphActivity.java
+++ app/src/main/java/com/hifunki/funki/module/login/PwdGraphActivity.java
@@ -2,12 +2,23 @@ package com.hifunki.funki.module.login;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.hifunki.funki.R;
 import com.hifunki.funki.base.activity.BaseActivity;
+import com.hifunki.funki.module.login.widget.PwdGraphView;
+import com.hifunki.funki.module.login.widget.ToolTitleBar;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
- *
  * 图形验证码界面
  *
  * @author monotone
@@ -16,7 +27,27 @@ import com.hifunki.funki.base.activity.BaseActivity;
  * @link
  * @since 2017-02-24 12:03:03
  */
-public class PwdGraphActivity extends BaseActivity {
+public class PwdGraphActivity extends BaseActivity implements View.OnClickListener {
+
+    @BindView(R.id.iv_pwd_country)
+    ImageView ivPwdCountry;
+    @BindView(R.id.tv_pwd_country)
+    TextView tvPwdCountry;
+    @BindView(R.id.llCounty)
+    LinearLayout llCounty;
+    @BindView(R.id.etIuputTel)
+    EditText etIuputTel;
+    @BindView(R.id.iv_pwd_graph)
+    ImageView ivPwdGraph;
+    @BindView(R.id.etIuputGraph)
+    EditText etIuputGraph;
+    @BindView(R.id.tvConfirm)
+    TextView tvConfirm;
+    @BindView(R.id.rlConfirm)
+    RelativeLayout rlConfirm;
+    @BindView(R.id.activity_pwd_graph)
+    LinearLayout activityPwdGraph;
+    private PwdGraphView pwdGraphView;
 
     public static void show(Context context) {
         context.startActivity(new Intent(context, PwdGraphActivity.class));
@@ -34,7 +65,10 @@ public class PwdGraphActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        pwdGraphView = PwdGraphView.getInstance();
+        pwdGraphView.setContext(this);
+        Bitmap bitmap = pwdGraphView.createBitmap();
+        ivPwdGraph.setImageBitmap(bitmap);
     }
 
     @Override
@@ -49,10 +83,35 @@ public class PwdGraphActivity extends BaseActivity {
 
     @Override
     protected void initTitleBar() {
-
+        ToolTitleBar.showLeftButton(this, activityPwdGraph, ToolTitleBar.BTN_TYPE_IMAGE, R.drawable.iv_back, this);
+        ToolTitleBar.showCenterButton(this, activityPwdGraph, ToolTitleBar.BTN_TYPE_TEXT, R.string.find_pwd, null);
     }
 
 
+    @OnClick({R.id.iv_pwd_country, R.id.tv_pwd_country, R.id.llCounty, R.id.iv_pwd_graph, R.id.tvConfirm, R.id.rlConfirm, R.id.activity_pwd_graph})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.rlTitleLeft:
+                finish();
+                break;
+            case R.id.iv_pwd_country:
+                break;
+            case R.id.tv_pwd_country:
+                break;
+            case R.id.llCounty:
+                break;
+            case R.id.iv_pwd_graph:
+                Bitmap bitmap = pwdGraphView.createBitmap();
+                ivPwdGraph.setImageBitmap(bitmap);
+                break;
+            case R.id.tvConfirm:
+                break;
+            case R.id.rlConfirm:
+                break;
+            case R.id.activity_pwd_graph:
+                break;
+        }
+    }
 }
 
 
