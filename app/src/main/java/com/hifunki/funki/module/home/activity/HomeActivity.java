3b303acab9c1ece71cc9f7b95981f6b5ad39a0b1
montotone <zgjxfuqingwang@gmail.com>
2017-04-10 16:36:10 +0800

    add-->yasea

--- app/src/main/java/com/hifunki/funki/module/home/activity/HomeActivity.java
+++ app/src/main/java/com/hifunki/funki/module/home/activity/HomeActivity.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 import com.hifunki.funki.R;
 import com.hifunki.funki.base.activity.BaseCoordinatorActivity;
 import com.hifunki.funki.common.FragmentConst;
+import com.hifunki.funki.module.MainActivity;
 import com.hifunki.funki.module.dynamic.activity.PostDynamicActivity;
 import com.hifunki.funki.module.home.fragment.HomeFragment;
 import com.hifunki.funki.module.home.fragment.HomeHotFragment;
@@ -30,7 +31,6 @@ import com.hifunki.funki.module.home.fragment.NavFragment;
 import com.hifunki.funki.module.home.fragment.StoreFragment;
 import com.hifunki.funki.module.home.inter.OnTabReselectListener;
 import com.hifunki.funki.module.home.widget.NavigationButton;
-import com.hifunki.funki.module.live.start.activity.StartLiveActivity;
 import com.hifunki.funki.module.me.MeFragment;
 import com.hifunki.funki.util.DisplayUtil;
 import com.hifunki.funki.util.PopWindowUtil;
@@ -158,7 +158,8 @@ public class HomeActivity extends BaseCoordinatorActivity implements NavFragment
             case R.id.ll_home_live://跳转到开启直播界面
                 pwdPopWindow.hidePopWindow();
                 SystemClock.sleep(500);
-                StartLiveActivity.show(this);
+                MainActivity.show(this);
+//                StartLiveActivity.show(this);
                 break;
         }
     }
