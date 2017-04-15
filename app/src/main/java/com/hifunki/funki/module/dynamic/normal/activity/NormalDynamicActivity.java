a9bbdc182045b0fa55fb376f16b103033c882efe
montotone <zgjxfuqingwang@gmail.com>
2017-04-15 18:38:15 +0800

    alter -->roomFragment private msg

---
+++ app/src/main/java/com/hifunki/funki/module/dynamic/normal/activity/NormalDynamicActivity.java
@@ -0,0 +1,42 @@
+package com.hifunki.funki.module.dynamic.normal.activity;
+
+import com.hifunki.funki.R;
+import com.hifunki.funki.base.activity.BaseActivity;
+
+/**
+ * 在此写用途
+ *
+ * @author monotone
+ * @version V1.0 <描述当前版本功能>
+ * @value com.hifunki.funki.module.dynamic.normal.activity.NormalDynamicActivity.java
+ * @link
+ * @since 2017-04-15 14:08:08
+ */
+public class NormalDynamicActivity extends BaseActivity {
+
+    @Override
+    protected int getViewResId() {
+        return R.layout.activity_normal_dynamic;
+    }
+
+    @Override
+    protected void initVariable() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void bindData() {
+
+    }
+
+    @Override
+    protected void bindData4NoNet() {
+
+    }
+
+}
