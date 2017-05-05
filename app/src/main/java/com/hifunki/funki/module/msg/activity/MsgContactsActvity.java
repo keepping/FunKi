639abc48fe52c50a0607195f2ac32ef2a1e6234e
montotone <zgjxfuqingwang@gmail.com>
2017-05-05 10:16:39 +0800

    add-->MsgContactsFragment

--- app/src/main/java/com/hifunki/funki/module/msg/activity/MsgContactsActvity.java
+++ app/src/main/java/com/hifunki/funki/module/msg/activity/MsgContactsActvity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import com.hifunki.funki.R;
 import com.hifunki.funki.base.activity.BaseActivity;
 import com.hifunki.funki.module.msg.adapter.MsgContactsAdapter;
+import com.hifunki.funki.module.msg.fragment.ContactsCloseFriendFragment;
 import com.hifunki.funki.module.msg.fragment.ContactsFansFragment;
 import com.hifunki.funki.module.msg.fragment.ContactsFriendFragment;
 import com.hifunki.funki.widget.bar.TopBarView;
@@ -20,7 +21,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class MsgContactsActvity extends BaseActivity implements ContactsFansFragment.OnFragmentInteractionListener, ContactsFriendFragment.OnFragmentInteractionListener {
+public class MsgContactsActvity extends BaseActivity implements ContactsFansFragment.OnFragmentInteractionListener, ContactsFriendFragment.OnFragmentInteractionListener, ContactsCloseFriendFragment.OnFragmentInteractionListener {
 
     @BindView(R.id.tbv_msg)
     TopBarView tbvMsg;
@@ -40,11 +41,13 @@ public class MsgContactsActvity extends BaseActivity implements ContactsFansFrag
     protected void initVariable() {
         //封装tab数据,数据和tab保持一致
         mTabTitle = new ArrayList<>();
+        mTabTitle.add("密友");
         mTabTitle.add("好友");
         mTabTitle.add("粉丝");
 
         tbMsgContacts.addTab(tbMsgContacts.newTab().setText(mTabTitle.get(0)));
         tbMsgContacts.addTab(tbMsgContacts.newTab().setText(mTabTitle.get(1)));
+        tbMsgContacts.addTab(tbMsgContacts.newTab().setText(mTabTitle.get(2)));
     }
 
     @Override
@@ -55,6 +58,8 @@ public class MsgContactsActvity extends BaseActivity implements ContactsFansFrag
     @Override
     protected void initAdapter() {
         super.initAdapter();
+
+
         MsgContactsAdapter adapter = new MsgContactsAdapter(getSupportFragmentManager(), mTabTitle);
         vpMsgContacts.setAdapter(adapter);
         tbMsgContacts.setupWithViewPager(vpMsgContacts);
